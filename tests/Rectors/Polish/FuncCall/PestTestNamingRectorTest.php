9ac43f4e1bf16477114b8475302b0d504707f624
Owen Voke <development@voke.dev>
2020-07-21 16:56:07 +0100

    test(polish): add test for PestTestNamingRector

---
+++ tests/Rectors/Polish/FuncCall/PestTestNamingRectorTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Pest\Drift\Testing\Rectors\Polish\FuncCall;
+
+use Iterator;
+use Pest\Drift\Testing\Rectors\PHPUnit\BasePHPUnitRectorTest;
+
+class PestTestNamingRectorTest extends BasePHPUnitRectorTest
+{
+    /**
+     * @dataProvider provideData()
+     */
+    public function test(string $file): void
+    {
+        $this->doTestFile($file);
+    }
+
+    public function provideData(): Iterator
+    {
+        return $this->yieldFilesFromDirectory(
+            __DIR__ . '/../../../fixtures/Polish/FuncCall/PestTestNamingRector'
+        );
+    }
+}
