36bc5b9f243e567729de39c2801e5aaee189db83
Owen Voke <development@voke.dev>
2020-09-01 12:45:40 +0100

    fix: update tests to work with latest Rector

--- tests/Rectors/Polish/BasePolishRectorTest.php
+++ tests/Rectors/Polish/BasePolishRectorTest.php
@@ -5,7 +5,12 @@ declare(strict_types=1);
 namespace Pest\Drift\Testing\Rectors\Polish;
 
 use Pest\Drift\Testing\BaseRectorTestCase;
+use Symplify\SmartFileSystem\SmartFileInfo;
 
 abstract class BasePolishRectorTest extends BaseRectorTestCase
 {
+    protected function provideConfigFileInfo(): ?SmartFileInfo
+    {
+        return new SmartFileInfo(__DIR__ . '/../../config/polish_rectors.php');
+    }
 }
--- tests/config/phpunit_rectors.php
+++ tests/config/phpunit_rectors.php
@@ -5,5 +5,5 @@ declare(strict_types=1);
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
-    $containerConfigurator->import(__DIR__ . '/../../config/*');
+    $containerConfigurator->import(__DIR__ . '/../../config/phpunit-to-pest.php');
 };
