6bf4c517a0cf9ebb55edc58f969927e6673b544a
Owen Voke <development@voke.dev>
2020-09-02 09:37:57 +0100

    ci: add additional Rector rulesets

--- rector-ci.php
+++ rector-ci.php
@@ -14,6 +14,9 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         SetList::DEAD_CODE,
         SetList::PHP_70,
         SetList::PHP_71,
+        SetList::PHP_72,
+        SetList::PHP_73,
+        SetList::PHP_74,
         SetList::SOLID,
     ]);
 
